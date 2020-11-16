651752defc1d3edffdcab634d20a3cbefda8dbb9
weixiaolin <weixiaolin@qq.com>
2020-11-16 16:11:41 +0800

    feat: 加入并发模式

--- src/myReact/ReactDOM.js
+++ src/myReact/ReactDOM.js
@@ -1,5 +1,8 @@
 import { TEXT_ELEMENT } from "./const"
 
+// 下一个浏览器空闲时间要执行的任务
+let nextUnitOfWork = null
+
 /**
  * 渲染函数，将vdom转为dom
  * @param {*} element react元素
@@ -20,10 +23,32 @@ function render(element, container) {
         })
 
     // 递归遍历子元素
-    element.props.children.forEach(child => render(child, dom))
+    element.props.children.forEach(child =>
+        render(child, dom)
+    )
 
     // 添加子元素
     container.appendChild(dom)
+}
+
+function workLoop(deadline) {
+    let shouldYield = false // 是否阻塞执行任务
+    while (nextUnitOfWork && !shouldYield) {
+        // 执行任务
+        nextUnitOfWork = performUnitOfWork(
+            nextUnitOfWork
+        )
+        // 剩余空闲时间不足一毫秒的时候暂停执行
+        shouldYield = deadline.timeRemaining() < 1
+    }
+    requestIdleCallback(workLoop)
+}
+
+// 浏览器处于空闲的时候会调用
+requestIdleCallback(workLoop)
+
+function performUnitOfWork(nextUnitOfWork){
+    // TODO
 }
 
 const ReactDOM = {
