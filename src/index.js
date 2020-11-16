a8ffdf49c135a523074ddde8958055eb8c14c550
weixiaolin <weixiaolin@qq.com>
2020-11-16 10:34:12 +0800

    feat: 替换jsx和render函数

--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+/**
+ * 下面👇用
+ * element 代表React元素
+ * node 代表DOM元素
+ */
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const element = React.createElement("h1", { title: 'foo' }, 'hello world')
+// console.log(element)
+
+const container = document.getElementById("root")
+
+const node = document.createElement(element.type)
+node['title'] = element.props.title
+const text = document.createTextNode('')
+text['nodeValue'] = element.props.children
+node.appendChild(text)
+container.appendChild(node)
